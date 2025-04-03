5b74d4035bd52bb018b3935675aa9d36ae87b414
Moshadi Hansamali <99788907+Moshadi@users.noreply.github.com>
2025-04-03 23:24:55 +1100

    Add files via upload

---
+++ task2_1Part2_224783796/task2_1Part2-main/2.1P/task2_1P/src/main/java/sit707_week2/Main.java
@@ -0,0 +1,9 @@
+package sit707_week2;
+
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        SeleniumOperations.coolplanet_registration_page("https://coolplanet.lk/account/register");
+    }
+}
